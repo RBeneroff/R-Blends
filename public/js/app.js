3a4f819fb9b8a9cbbaefcf9b1abd42c0b31e17c0
Rachel Beneroff <rbbeneroff@gmail.com>
2016-11-10 15:51:03 -0500

    index.js and bower components

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+(function() {
+  angular
+    .module('BlenderApp', [])
+    .controller('BlenderController', function($http, $state) {
+
+      var self = this;
+      var rootUrl = 'http://localhost:3001'
+
+
+
+    }); //Controller
+})() //IIFE
---
+++ public/js/router.js
@@ -0,0 +1,44 @@
+(function() {
+  angular
+    .module('BlenderApp', ['ui.router'])
+    .config(MainRouter);
+
+  MainRouter.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+
+  function MainRouter($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'home.html'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'login.html'
+    })
+    .state('signup', {
+      url: '/signup',
+      templateUrl: 'signup.html'
+    })
+    .state('settings', {
+      url: '/settings',
+      templateUrl: 'settings.html'
+    })
+    .state('explore', {
+      url: '/explore',
+      templateUrl: 'explore.html'
+    })
+    .state('blend', {
+      url: '/blend',
+      templateUrl: 'blend.html'
+    })
+
+    $urlRouterProvider.otherwise('/');
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    })
+
+
+  } //MainRouter
+})() //IIFE
